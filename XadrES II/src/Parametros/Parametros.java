e0b6fc8894b44d41792e878527d240b9668e8a3e
CephBr@gmail.com <CephBr@gmail.com@e213d49a-795f-fc1f-dd2b-8a6015789a8b>
2011-10-16 01:18:18 +0000

    Tarefas Feitas: Implementação do turno -> Primeiro o branco, depois o preto 		    Implementação do jogador CPU -> Um jogador não pode mexer as peças pretas caso esteja jogando                                                                                  contra o CPU                          Implementação do P1 vs P2 -> O jogador 1 não pode mexer as peças pretas, nem o o J2 pode mexer as peças brancas

---
+++ XadrES II/src/Parametros/Parametros.java
@@ -0,0 +1,66 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Parametros;
+
+import Interface.Tabuleiro;
+
+/**
+ *
+ * @author Ceph
+ */
+public class Parametros {
+    String nomeJogador1;
+    String nomeJogador2;
+    Tabuleiro tabuleiro;
+    String turno;  
+    Boolean temCpu = Boolean.FALSE;
+    
+    public String vezDeQuem(){
+        return turno;
+    }
+    
+    public void passaVez(){
+        if (turno.equals(Constantes.BRANCO)) 
+            turno = Constantes.PRETO;
+        else
+            turno= Constantes.BRANCO;
+    }
+    
+    public void defineTurno(String turno){
+        this.turno=turno;
+    }
+    
+    
+    public Boolean temCpu() {
+        return temCpu;
+    }
+
+    public void setTemCpu(Boolean temCpu) {
+        this.temCpu = temCpu;
+    }
+    
+    
+    
+    public String getNomeJogador1() {
+        return nomeJogador1;
+    }
+
+    public void setNomeJogador1(String nomeJogador1) {
+        this.nomeJogador1 = nomeJogador1;
+    }
+
+    public String getNomeJogador2() {
+        return nomeJogador2;
+    }
+
+    public void setNomeJogador2(String nomeJogador2) {
+        this.nomeJogador2 = nomeJogador2;
+    }
+    
+    
+            
+         
+    
+}
