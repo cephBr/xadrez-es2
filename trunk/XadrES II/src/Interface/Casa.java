41994581a9313d83b19eac980130f122dad5edbb
leandrogaluzzi@gmail.com <leandrogaluzzi@gmail.com@e213d49a-795f-fc1f-dd2b-8a6015789a8b>
2011-11-13 19:49:37 +0000

    Juntei a IA burra na última versão que tem o menu suspenso funcionando. tem um erro que sempre que a IA joga dá uma mensagem de posição inválida. E depois de um tempo trava do nada. Preciso de assistência nessa parte.

---
+++ trunk/XadrES II/src/Interface/Casa.java
@@ -0,0 +1,17 @@
+
+package Interface;
+
+/**
+ *
+ * @author Thiago
+ */
+public class Casa {
+    
+    public Peca peca;
+    public Boolean ocupada;
+    public int x0,x1,y0,y1;
+    public int posX,posY;
+    public int prioridadeDesenho;
+    
+}
+
